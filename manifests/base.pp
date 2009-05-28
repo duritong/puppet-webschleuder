c61292245674639c74eb77c8bc3cc24f4a51e6ec
duritong <peter.meier@immerda.ch>
2009-05-28 21:04:40 +0200

    added a bunch of dependecies and the configfile

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,6 +1,12 @@
 class webschleuder::base {
   include schleuder
 
+  include rubygems::activerecord
+  include rubygems::bcrypt
+  include rubygems::camping
+  include rubygems::markaby
+  include rubygems::rack
+
   if $webschleuder_install_dir == '' {
     $webschleuder_install_dir  = '/opt/webschleuder'
   }
@@ -9,6 +15,13 @@ class webschleuder::base {
     git_repo => 'git://git.immerda.ch/webschleuder.git',
     projectroot => $webschleuder_install_dir,
     cloneddir_group => 'schleuder',
-    require => User::Managed['schleuder'],
+    require => [ User::Managed['schleuder'], Package['camping'], Package['markaby'], Package['activerecord'] ]
+  }
+
+  file{'/etc/schleuder/webschleuder.conf':
+    source => [ "puppet://$server/files/webschleuder/config/${fqdn}/webschleuder.conf",
+                "puppet://$server/files/webschleuder/config/webschleuder.conf",
+                "puppet://$server/webschleuder/config/webschleuder.conf" ],
+    owner => root, group => schleuder, mode => 0640;
   }
 }
