9164952dc98d7772f00ff792bf4d5c58a6f0a1e5
duritong <peter.meier@immerda.ch>
2009-05-29 02:34:26 +0200

    added passenger support stuff

---
+++ manifests/passenger.pp
@@ -0,0 +1,15 @@
+class webschleuder::passenger {
+  include webschleuder
+  include passenger::apache
+
+  file{ [ '/var/schleuderlists/webschleuder_data', '/var/schleuderlists/webschleuder_data/tmp' ]:
+    ensure => directory,
+    require => User::Managed['schleuder'],
+    owner => schleuder, group => 0, mode => 0600;
+  }
+  file{'/var/schleuderlists/webschleuder_data/camping.db':
+    ensure => present,
+    require => User::Managed['schleuder'],
+    owner => schleuder, group => 0, mode => 0600;
+  }
+}
