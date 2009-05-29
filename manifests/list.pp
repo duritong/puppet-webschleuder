ba9d85854defbd4badd562a7c1f698967ba300d9
duritong <peter.meier@immerda.ch>
2009-05-29 02:35:47 +0200

    added define to manage for webschleuder

---
+++ manifests/list.pp
@@ -0,0 +1,49 @@
+# force_password: wether we'd like to enforce a given password, 
+#   which users can't change. Only possible for encrypted passwords
+#   - default: false
+define webschleuder::list(
+  $ensure = present,
+  $password,
+  $password_iscrypted = true,
+  $force_password = false
+){
+  include webschleuder
+
+  if ($password_iscrypted == false) and $force_password {
+    fail("it's not possible to enforcen an unencrypted password for ${name} on ${fqdn}")
+  }
+
+  if $ensure == present {
+    if $force_password {
+      $force_str = "-override"
+    } else {
+      $force_str = ""
+    }
+    if $password_iscrypted {
+      $pwd_str = "-encrypted"
+    } else {
+      $pwd_str = ""
+    }
+
+    exec{"manage_webschleuder_${name}":
+      command => "ruby /opt/webschleuder/contrib/enable_webschleuder.rb ${name} ${password} ${pwd_str} {force_str}",
+      require => Exec["manage_schleuder_list_${name}"],
+    }
+    
+    if $force_password {
+      if $password_iscrypted {
+        Exec["manage_webschleuder_${name}"]{
+          unless => "grep -qE '${password}' /var/schleuderlists/${name}/web.conf",
+        }
+      }
+    } else {
+      Exec["manage_webschleuder_${name}"]{
+        creates => "/var/schleuderlists/${name}/web.conf",
+      }
+    }
+  } else {
+    file{"/var/schleuderlists/${name}/web.conf":
+      ensure => absent,
+    }
+  } 
+}
